83fbc33d6137d99c05bb078183d0331d4ac767e8
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-04-25 00:08:24 +0300

    Refactor

--- src/cbs/high_level.rs
+++ src/cbs/high_level.rs
@@ -92,9 +92,6 @@ impl<'a> ConflictTreeNode<'a> {
                 if agent_locations[&location].len() > 1 {
                     for (j, agent) in agent_locations[&location].iter().enumerate() {
                         for agent2 in agent_locations[&location][..j].iter() {
-                            if agent2 == agent {
-                                continue;
-                            }
                             if i > 0
                                 && i < self.paths[agent2].len()
                                 && self.paths[agent2][i] == self.paths[agent][i - 1]
@@ -254,41 +251,4 @@ impl<'a> CBS<'a> {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_high_level() {
-        let agents = vec![
-            Agent {
-                id: "a".to_string(),
-                start: (0, 0),
-                goal: (9, 9),
-            },
-            Agent {
-                id: "b".to_string(),
-                start: (0, 1),
-                goal: (9, 8),
-            },
-        ];
-        let constraints = vec![
-            Box::new(Constraint {
-                agent: &agents[0],
-                time: 0,
-                location: (1, 0),
-            }),
-            Box::new(Constraint {
-                agent: &agents[1],
-                time: 0,
-                location: (8, 0),
-            }),
-        ];
-        let precomputed_paths = HashMap::<&Agent, Vec<(i32, i32)>>::new();
-        let ctn = ConflictTreeNode::new(agents.iter().collect(), constraints, precomputed_paths);
-        assert_eq!(ctn.conflicts.len(), 1);
-        let expanded = ctn.expand();
-        assert_eq!(expanded.len(), 2);
-        assert_eq!(expanded[0].constraints.len(), 3);
-        assert_eq!(expanded[1].constraints.len(), 3);
-    }
-}
+mod tests;
---
+++ src/cbs/high_level/tests.rs
@@ -0,0 +1,38 @@
+use super::*;
+
+#[test]
+fn test_high_level() {
+    let agents = vec![
+        Agent {
+            id: "a".to_string(),
+            start: (0, 0),
+            goal: (9, 9),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (0, 1),
+            goal: (9, 8),
+        },
+    ];
+    let constraints = vec![
+        Box::new(Constraint {
+            agent: &agents[0],
+            time: 0,
+            location: (1, 0),
+        }),
+        Box::new(Constraint {
+            agent: &agents[1],
+            time: 0,
+            location: (8, 0),
+        }),
+    ];
+    let precomputed_paths = HashMap::<&Agent, Vec<(i32, i32)>>::new();
+    let ctn = ConflictTreeNode::new(agents.iter().collect(), constraints, precomputed_paths);
+    assert_eq!(ctn.conflicts.len(), 1);
+    let expanded = ctn.expand();
+    assert_eq!(expanded.len(), 2);
+    assert_eq!(expanded[0].constraints.len(), 3);
+    assert_eq!(expanded[1].constraints.len(), 3);
+    assert_eq!(expanded[0].constraints[2].time, 17);
+    assert_eq!(expanded[0].conflicts.len(), 0);
+}
