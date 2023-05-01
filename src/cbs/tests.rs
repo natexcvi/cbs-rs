a3eb077a16285707e4bbbd532ad7e728bab1deb6
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-05-01 13:48:39 +0300

    Fix formatting

--- src/cbs/tests.rs
+++ src/cbs/tests.rs
@@ -8,120 +8,130 @@ use rstest::rstest;
         bypassing_conflicts: true,
     }),
     vec![
-Agent {
-    id: "a".to_string(),
-    start: (0, 0),
-    goal: (9, 9),
-},
-Agent {
-    id: "b".to_string(),
-    start: (0, 1),
-    goal: (9, 8),
-},
-], Grid {
-width: 10,
-height: 10,
-obstacles: vec![],
-goal: (9, 9),
-}, vec![19, 17])]
+        Agent {
+            id: "a".to_string(),
+            start: (0, 0),
+            goal: (9, 9),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (0, 1),
+            goal: (9, 8),
+        },
+    ], Grid {
+        width: 10,
+        height: 10,
+        obstacles: vec![],
+        goal: (9, 9),
+    },
+    vec![19, 17],
+)]
 #[case::opposite_corners(
     Some(CBSOptimisationConfig {
         priotising_conflicts: true,
         bypassing_conflicts: true,
     }),
     vec![
-Agent {
-    id: "a".to_string(),
-    start: (9, 9),
-    goal: (0, 0),
-},
-Agent {
-    id: "b".to_string(),
-    start: (0, 0),
-    goal: (9, 9),
-},
-], Grid {
-width: 10,
-height: 10,
-obstacles: vec![],
-goal: (9, 9),
-}, vec![19, 19])]
+        Agent {
+            id: "a".to_string(),
+            start: (9, 9),
+            goal: (0, 0),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (0, 0),
+            goal: (9, 9),
+        },
+    ], Grid {
+        width: 10,
+        height: 10,
+        obstacles: vec![],
+        goal: (9, 9),
+    },
+    vec![19, 19],
+)]
 #[case::crowded(
     Some(CBSOptimisationConfig {
         priotising_conflicts: true,
         bypassing_conflicts: true,
     }),
     vec![
-Agent {
-    id: "a".to_string(),
-    start: (0, 0),
-    goal: (1, 1),
-},
-Agent {
-    id: "b".to_string(),
-    start: (1, 0),
-    goal: (0, 0),
-},
-Agent {
-    id: "c".to_string(),
-    start: (0, 1),
-    goal: (1, 0),
-},
-], Grid {
-width: 2,
-height: 2,
-obstacles: vec![],
-goal: (0, 0),
-}, vec![4, 2, 3])] // TODO: confirm this case
+        Agent {
+            id: "a".to_string(),
+            start: (0, 0),
+            goal: (1, 1),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (1, 0),
+            goal: (0, 0),
+        },
+        Agent {
+            id: "c".to_string(),
+            start: (0, 1),
+            goal: (1, 0),
+        },
+    ], Grid {
+        width: 2,
+        height: 2,
+        obstacles: vec![],
+        goal: (0, 0),
+    },
+    vec![4, 2, 3],
+)] // TODO: confirm this case
 #[case::crowded_with_obstacles(
     Some(CBSOptimisationConfig {
         priotising_conflicts: true,
         bypassing_conflicts: true,
     }),
     vec![
-Agent {
-    id: "a".to_string(),
-    start: (0, 0),
-    goal: (2, 2),
-},
-Agent {
-    id: "b".to_string(),
-    start: (1, 0),
-    goal: (0, 0),
-},
-Agent {
-    id: "c".to_string(),
-    start: (0, 1),
-    goal: (1, 0),
-},
-], Grid {
-width: 3,
-height: 3,
-obstacles: vec![low_level::LocationTime { location: (1,1), time: 2 }],
-goal: (0, 0),
-}, vec![5, 2, 3])]
+        Agent {
+            id: "a".to_string(),
+            start: (0, 0),
+            goal: (2, 2),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (1, 0),
+            goal: (0, 0),
+        },
+        Agent {
+            id: "c".to_string(),
+            start: (0, 1),
+            goal: (1, 0),
+        },
+    ], Grid {
+        width: 3,
+        height: 3,
+        obstacles: vec![low_level::LocationTime { location: (1,1), time: 2 }],
+        goal: (0, 0),
+    },
+    vec![5, 2, 3],
+)]
 #[case::must_wait(
     Some(CBSOptimisationConfig {
         priotising_conflicts: true,
         bypassing_conflicts: true,
     }),
     vec![
-Agent {
-    id: "a".to_string(),
-    start: (0, 1),
-    goal: (2, 1),
-},
-Agent {
-    id: "b".to_string(),
-    start: (1, 0),
-    goal: (1, 2),
-},
-], Grid {
-width: 3,
-height: 3,
-obstacles: vec![low_level::LocationTime { location: (2,0), time: -1 }, low_level::LocationTime { location: (0,2), time: -1 }],
-goal: (0, 0),
-}, vec![3, 4])]
+        Agent {
+            id: "a".to_string(),
+            start: (0, 1),
+            goal: (2, 1),
+        },
+        Agent {
+            id: "b".to_string(),
+            start: (1, 0),
+            goal: (1, 2),
+        },
+    ], Grid {
+        width: 3,
+        height: 3,
+        obstacles: vec![low_level::LocationTime { location: (2,0), time: -1 }, low_level::LocationTime { location: (0,2), time: -1 }],
+        goal: (0, 0),
+    },
+    vec![3, 4],
+)]
 fn test_cbs(
     #[case] optimisation_config: Option<CBSOptimisationConfig>,
     #[case] agents: Vec<Agent>,
