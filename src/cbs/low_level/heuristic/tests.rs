b6de5389e76dde2f52397e75788e71bcc7cce01a
natexcvi <62808031+natexcvi@users.noreply.github.com>
2023-05-29 12:51:30 +0300

    Dynamic manhatten prop tests

--- src/cbs/low_level/heuristic/tests.rs
+++ src/cbs/low_level/heuristic/tests.rs
@@ -1,6 +1,7 @@
 use rstest::rstest;
 
 use super::*;
+use proptest::prelude::*;
 
 #[rstest]
 #[case::simple(
@@ -69,15 +70,41 @@ use super::*;
     (4, 0),
     4.0,
 )]
-fn test_true_distance(
-    #[case] grid: Grid,
-    #[case] query: (i32, i32),
-    #[case] true_distance: f64,
-) {
+fn test_true_distance(#[case] grid: Grid, #[case] query: (i32, i32), #[case] true_distance: f64) {
     let td = TrueDistance::new(Rc::new(grid), (0, 0));
     let h = td.h(&LocationTime {
         location: query,
         time: 0,
     });
     assert_eq!(h, true_distance);
+}
+
+prop_compose! {
+    fn arb_location()(x in 0..5000, y in 0..5000) -> Location {
+        (x, y)
+    }
+}
+
+proptest! {
+    #[rstest]
+    fn test_dynamic_goal_manhatten_distance(goal in arb_location(), second_goal in arb_location(), query in arb_location()) {
+        let td = DynamicGoalManhattanDistance::new(goal);
+        let h = td.h(&LocationTime {
+            location: query,
+            time: 0,
+        });
+        assert_eq!(
+            h,
+            (goal.0 - query.0).abs() as f64 + (goal.1 - query.1).abs() as f64
+        );
+        td.set_goal(second_goal);
+        let h = td.h(&LocationTime {
+            location: query,
+            time: 0,
+        });
+        assert_eq!(
+            h,
+            (second_goal.0 - query.0).abs() as f64 + (second_goal.1 - query.1).abs() as f64
+        );
+    }
 }
